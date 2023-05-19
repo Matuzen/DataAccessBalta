7a90f36b8b16b5c61ef0d11946349350342fb1b0
Matheus Alexandre <matheuscurses@gmail.com>
2023-05-19 11:28:14 -0300

    TRansactions e Like

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        const string connectionString = "Server=localhost\\SQLEXPRESS,1433;Integrated Security=SSPI";
+        // Vamos nos conectar ao banco usando o pacote Microsoft.Data.SqlClient (NUGGET)
+
+        // Usando o pacote que criamos para se conectar ao banco de dados
+        var connection = new SqlConnection(); // Objeto de conexão com o banco
+    }
+}
